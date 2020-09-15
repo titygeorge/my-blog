f4d34e9e218cd42c9734a452e307a32525b229dc
tity <tity.george@seqato.com>
2020-09-15 09:00:48 +0530

    new changes

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,42 +1,59 @@
 import React, { Component } from 'react';
 import './Category.css';
+import { Link } from 'react-router-dom';
 // import CategoryDetails from '../../components/CategoryDetails/CategoryDetails';
 // import history from '../history';
 
 class Category extends Component {
 	constructor(props) {
     	super(props);
-	    this.eventClick = this.eventClick.bind(this);
+    	this.handleClick = this.handleClick.bind(this);
 	  }
 
-  eventClick(event) {
-    // var txtId = event.currentTarget.id;
-    // <CategoryDetails></CategoryDetails>
+
+  handleClick = value => () => {
+  	this.clickedId = value;
+  	const newTo = { 
+	  pathname: "/category-details", 
+	  data: [{name: this.clickedId }]
+	};
+  	// pathname: "/category-details";
+  	// data: [{name: this.clickedId }]
+    console.log('this is:', newTo);
   }
+
 
 	render() {
 		return (
 			<div className="container-fluid category">
 				<div className="row col-sm-12">
 					<div className="col-sm-3">
-						<div id="arts" className="arts flex bg" >
-							<h3 className="imgtxt cWhite">Arts</h3>
-						</div>
+						<Link to={{pathname: "/category-details", data: this.clickedId}}>
+							<div id="arts" className="arts flex bg" onClick={this.handleClick('arts')}>
+								<h3 className="imgtxt cWhite">Arts</h3>
+							</div>
+						</Link>
 					</div>
 					<div className="col-sm-3">
-						<div id="sports" className="sports flex bg" onClick={this.eventClick}>
-							<h3 className="imgtxt cRoyal">Sports</h3>
-						</div>
+						<Link to={{pathname: "/category-details", data: this.clickedId}}>
+							<div id="sports" className="sports flex bg" onClick={this.handleClick('sports')}>
+								<h3 className="imgtxt cRoyal">Sports</h3>
+							</div>
+						</Link>
 					</div>
 					<div className="col-sm-3">
-						<div id="education" className="education flex bg" onClick={this.eventClick}>
-							<h3 className="imgtxt cBlack">Education</h3>
-						</div>
+						<Link to={{pathname: "/category-details", data: this.clickedId}}>
+							<div id="education" className="education flex bg" onClick={this.handleClick('education')}>
+								<h3 className="imgtxt cBlack">Education</h3>
+							</div>
+						</Link>
 					</div>
 					<div className="col-sm-3">
-						<div id="travel" className="travel flex bg" onClick={this.eventClick}>
-							<h3 className="imgtxt">Travel</h3>
-						</div>					
+						<Link to={{pathname: "/category-details", data: this.clickedId}}>
+							<div id="travel" className="travel flex bg" onClick={this.handleClick('travel')}>
+								<h3 className="imgtxt">Travel</h3>
+							</div>			
+						</Link>		
 					</div>
 				</div>
 			</div>
--- src/components/CategoryDetails/CategoryDetails.js
+++ src/components/CategoryDetails/CategoryDetails.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
 import './CategoryDetails.css';
+import data from "../../data/data"; 
 
 class CategoryDetails extends Component {
+	constructor(props) {
+		super(props);
+	console.log("data", this.props)
+	}
 	render() {
 		return (
-			<div className="container ">
-				<ul>
-					<li><a href="">List 1</a></li>
-					<li><a href="">List 2</a></li>
-					<li><a href="">List 3</a></li>
-				</ul>
-			</div>
+			<div>
+                {
+					data.category1.map((category, i) => {
+						return (
+							<div key={i}>
+								{this.props.data}
+								<div className="container">
+									<div className="row">
+										<div className="col-sm-6">
+											<h1>{category.title}</h1>
+										</div>
+										<div className="col-sm-6">
+											<a href={category.url}>
+												<img src={category.url} width="200px" height="200px" alt={category.title} />
+											</a>
+										</div>
+									</div>
+									
+									
+									<div className="row">
+										<div className="col-sm-12">
+											<p>{category.description}</p>
+										</div>
+											
+									</div>
+								</div>
+							</div>
+						);
+					})
+				}
+            </div>
+
 		);
 	}
 }
